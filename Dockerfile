edb44b8ed39e627fb06d3e06d0d64cb890b53854
Jey Puget Gil <LinkJerome@gmail.com>
2024-10-17 09:23:10 +0200

    Added data_destination env variable in Dockerfile and README.md

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM eclipse-temurin:11
 LABEL maintainer="vcity"
 LABEL description="This is a docker image for the BSBM project"
+
 WORKDIR /app
 COPY . /app
+
+ENV DATA_DESTINATION=data
+
 ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,15 @@ if [ $# == 0 ]; then
 fi
 
 command=$1
+data_destination=$DATA_DESTINATION
 
-# create data folder if it doesn't exist
-mkdir -p data
-chmod 777 data
+# create data destination folder if it doesn't exist
+mkdir -p $data_destination
+chmod 777 $data_destination
 
 if [ "$command" = "generate-n" ]; then
     ./$@
-    mv dataset*.ttl data
+    mv dataset*.ttl $data_destination
 elif [ "$command" = "generate" ]; then
     ./$@
 
@@ -25,25 +26,25 @@ elif [ "$command" = "generate" ]; then
     fn=$(echo $@ | grep -oP '(?<=-fn )[^ ]+' || echo "dataset")
     # get the -s parameter value, if it exists else set it to "nt"
     s=$(echo $@ | grep -oP '(?<=-s )[^ ]+' || echo "nt")
-    mv $fn*.$s data
+    mv $fn*.$s $data_destination
 
     # check if the -ud parameter exists
     if [[ $@ == *"-ud"* ]]; then
         udf=$(echo $@ | grep -oP '(?<=-udf )[^ ]+' || echo "dataset_update")
-        mv $udf*.nt data
+        mv $udf*.nt $data_destination
     fi
 elif [ "$command" = "testdriver" ]; then
     ./$@
 
     # get the -o parameter value, if it exists else set it to "dataset"
     o=$(echo $@ | grep -oP '(?<=-o )[^ ]+' || echo "benchmark_result.xml")
-    mv $o data
+    mv $o $data_destination
 elif [ "$command" = "qualification" ]; then
     ./$@
 
     # get the -ql parameter value, if it exists else set it to "dataset"
     ql=$(echo $@ | grep -oP '(?<=-ql )[^ ]+' || echo "qual.log")
-    mv $ql data
+    mv $ql $data_destination
 else
     echo "Invalid command: $command"
     exit 1
